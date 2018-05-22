1ba727708f470641d1e5f29ca6a9aac2ee6c680d
Lisa Mac <lisamac421@gmail.com>
2018-05-22 18:49:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function nestedTarget(){
   return document.getElementById('nested').getElementsByClassName("target")[0]
 
 }
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
