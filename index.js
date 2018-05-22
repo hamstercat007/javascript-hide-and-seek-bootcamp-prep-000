7f5302fb6bcd1cf89c4efa611b3638aa94baab18
Lisa Mac <lisamac421@gmail.com>
2018-05-22 19:09:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nestedTarget(){
 function increaseRankBy(n) {
   var rankedList = document.getElementsByClassName("ranked-list")
   for (let i = 0; i < rankedList.length; i++) {
-  rankedList[i].innerHTML = (i + 1).toString()
+  rankedList[i].innerHTML = (i + n).toString()
   }
 }
 
