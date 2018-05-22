13833a85985985a06d74f3e3d9f12da3cee0bc6b
Lisa Mac <lisamac421@gmail.com>
2018-05-22 19:39:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').getElementsByClassName("target")[0]
+
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var rankedList = document.getElementsByClassName("ranked-list")
+  for (let i = 0; i < rankedList.length; i++) {
+  rankedList[i].innerHTML = (parseInt(rankedList[i].innerHTML) + n).toString()
+  }
+}
+
+function deepestChild(){
+ var currentNode = document.getElementById('grand-node')
+ while(true){
+   if (currentNode.children.length === 0){
+     return currentNode
+   }
+   else currentNode = currentNode.children[0]
+ }
+}
